c0c08ab67650a4f52950a8b7365ebb5fce3b7115
Arno Zura <d4kir92@gmail.com>
2018-04-05 01:07:53 +0200

    V.: 0.9.61
    V.: 0.9.61

--- yourrp/entities/entities/yrp_button2/cl_init.lua
+++
@@ -1,7 +0,0 @@
---Copyright (C) 2017-2018 Arno Zura ( https://www.gnu.org/licenses/gpl.txt )
-
-include('shared.lua')
-
-function ENT:Draw()
-  self:DrawModel()
-end
--- yourrp/gamemode/integration/darkrp/entity/client.lua
+++
@@ -1,9 +0,0 @@
---Copyright (C) 2017-2018 Arno Zura ( https://www.gnu.org/licenses/gpl.txt )
-
-local Entity = FindMetaTable( "Entity" )
-
-function Entity:drawOwnableInfo()
-  --Description: Draw the ownability information on a door or vehicle.
-  printGM( "darkrp", "drawOwnableInfo()" )
-  printGM( "darkrp", DarkRP._not )
-end
