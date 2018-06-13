0fa937ef19a3c1da36a3eb64346c93f78c423fb2
Arno Zura <d4kir92@gmail.com>
2018-06-13 22:45:17 +0200

    test

--- yourrp/gamemodes/yourrp/entities/entities/yrp_teleporter/cl_init.lua
+++
@@ -1,9 +0,0 @@
---Copyright (C) 2017-2018 Arno Zura ( https://www.gnu.org/licenses/gpl.txt )
-
-include("shared.lua")
-
-function ENT:Draw()
-  if LocalPlayer():GetPos():Distance( self:GetPos() ) < 2000 then
-    self:DrawModel()
-  end
-end
