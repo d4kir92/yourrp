3202ff7cbd3b48cc63fd317dd4b35d988448e1f2
darthginger <admin@diezuras.de>
2018-06-22 19:22:42 +0200

    refactor dyrpdblist

--- gamemode/yrp/apis/derma/dyrpdblist.lua
+++ gamemode/yrp/apis/derma/dyrpdblist.lua
@@ -1,51 +1,50 @@
 --Copyright (C) 2017-2018 Arno Zura ( https://www.gnu.org/licenses/gpl.txt )
-
 local PANEL = {}
 
-function paintDBButton( self, pw, ph, color, text )
+function paintDBButton(self, pw, ph, color, text)
 	local _color = color
+
 	if self:IsHovered() then
-		_color = Color( 255, 255, 0 )
+		_color = Color(255, 255, 0)
 	end
-	draw.RoundedBox( 0, 0, 0, pw, ph, _color )
-	surfaceText( text, "SettingsNormal", pw/2, ph/2, Color( 255, 255, 255 ), 1, 1 )
+
+	draw.RoundedBox(0, 0, 0, pw, ph, _color)
+	surfaceText(text, "SettingsNormal", pw / 2, ph / 2, Color(255, 255, 255), 1, 1)
 end
 
 function PANEL:Init()
 	local _pnl = self
+	--[[ Header ]]
+	--
+	self.header = createD("DPanel", self, self:GetWide(), ctr(50), 0, 0)
 
-	--[[ Header ]]--
-	self.header = createD( "DPanel", self, self:GetWide(), ctr( 50 ), 0, 0 )
-	function self.header:Paint( pw, ph )
+	function self.header:Paint(pw, ph)
 		--draw.RoundedBox( 0, 0, 0, pw, ph, Color( 255, 255, 0 ) )
 	end
+
 	self.panels = {}
-	self.header.add = createD( "DButton", self.header, self:GetWide()/5, self:GetTall(), 0, 0 )
-	self.header.add:SetText( "" )
-	function self.header.add:Paint( pw, ph )
-		paintDBButton( self, pw, ph, Color( 0, 255, 0 ), "+" )
+	self.header.add = createD("DButton", self.header, self:GetWide() / 5, self:GetTall(), 0, 0)
+	self.header.add:SetText("")
+
+	function self.header.add:Paint(pw, ph)
+		paintDBButton(self, pw, ph, Color(0, 255, 0), "+")
 	end
+
 	function self.header.add:DoClick()
 		_pnl:AddFunction()
 	end
-	--[[
-	self.header.dup = createD( "DButton", self.header, 3*(self:GetWide()/5), self:GetTall(), self:GetWide()/5, 0 )
-	self.header.dup:SetText( "" )
-	function self.header.dup:Paint( pw, ph )
-		if _pnl.uid != nil then
-			paintDBButton( self, pw, ph, Color( 0, 0, 255 ), lang_string( "duplicate" ) )
+
+	self.header.rem = createD("DButton", self.header, self:GetWide() / 5, self:GetTall(), 4 * (self:GetWide() / 5), 0)
+	self.header.rem:SetText("")
+
+	function self.header.rem:Paint(pw, ph)
+		if _pnl.uid ~= nil then
+			paintDBButton(self, pw, ph, Color(255, 0, 0), "-")
 		end
 	end
-	]]--
-	self.header.rem = createD( "DButton", self.header, self:GetWide()/5, self:GetTall(), 4*(self:GetWide()/5), 0 )
-	self.header.rem:SetText( "" )
-	function self.header.rem:Paint( pw, ph )
-		if _pnl.uid != nil then
-			paintDBButton( self, pw, ph, Color( 255, 0, 0 ), "-" )
-		end
-	end
+
 	function self.header.rem:DoClick()
-		if _pnl.uid != nil then
+		if _pnl.uid ~= nil then
 			_pnl:RemoveFunction()
 			_pnl.uid = nil
 		end
@@ -55,51 +54,60 @@ function PANEL:Init()
 		local _w = self:GetWide()
 		local _x = self.rem:GetPos()
 
-		if _x != 3*(_w/5) then
-			self.rem:SetPos( 4*(_w/5), 0 )
+		if _x ~= 3 * (_w / 5) then
+			self.rem:SetPos(4 * (_w / 5), 0)
 		end
 
-		if self.add:GetWide() != _w/5 then
-			self.add:SetWide( _w/5 )
-		end
-		if self.rem:GetWide() != _w/5 then
-			self.rem:SetWide( _w/5 )
+		if self.add:GetWide() ~= _w / 5 then
+			self.add:SetWide(_w / 5)
 		end
 
-		if self.add:GetTall() != self:GetTall() then
-			self.add:SetTall( self:GetTall() )
+		if self.rem:GetWide() ~= _w / 5 then
+			self.rem:SetWide(_w / 5)
 		end
-		if self.rem:GetTall() != self:GetTall() then
-			self.rem:SetTall( self:GetTall() )
+
+		if self.add:GetTall() ~= self:GetTall() then
+			self.add:SetTall(self:GetTall())
+		end
+
+		if self.rem:GetTall() ~= self:GetTall() then
+			self.rem:SetTall(self:GetTall())
 		end
 	end
 
-	--[[ List Header ]]--
-	self.listheader = createD( "DPanel", self, self:GetWide(), ctr( 50 ), 0, 0 )
+	--[[ List Header ]]
+	--
+	self.listheader = createD("DPanel", self, self:GetWide(), ctr(50), 0, 0)
 	self.listheader.textpre = ""
 	self.listheader.text = "UNNAMED"
 	self.listheader.textpos = ""
-	function self.listheader:Paint( pw, ph )
-		draw.RoundedBox( 0, 0, 0, pw, ph, Color( 0, 255, 0 ) )
-		surfaceText( self.textpre .. " " .. lang_string( self.text ) .. " " .. self.textpos, "SettingsNormal", pw/2, ph/2, Color( 255, 255, 255 ), 1, 1 )
+
+	function self.listheader:Paint(pw, ph)
+		draw.RoundedBox(0, 0, 0, pw, ph, Color(0, 255, 0))
+		surfaceText(self.textpre .. " " .. lang_string(self.text) .. " " .. self.textpos, "SettingsNormal", pw / 2, ph / 2, Color(255, 255, 255), 1, 1)
 	end
 
-	--[[ List ]]--
-	self.list = createD( "DPanelList", self, self:GetWide(), self:GetTall() - ctr( 110 ), 0, 0 )
-	self.list:EnableVerticalScrollbar( true )
-	self.list:SetSpacing( 2 )
+	--[[ List ]]
+	--
+	self.list = createD("DPanelList", self, self:GetWide(), self:GetTall() - ctr(110), 0, 0)
+	self.list:EnableVerticalScrollbar(true)
+	self.list:SetSpacing(2)
+
 	--self.list:SetNoSizing( true )
-	function self.list:Paint( pw, ph )
-		draw.RoundedBox( 0, 0, 0, pw, ph, Color( 100, 100, 100 ) )
+	function self.list:Paint(pw, ph)
+		draw.RoundedBox(0, 0, 0, pw, ph, Color(100, 100, 100))
 	end
 
-	--[[ Public functions ]]--
-	function self:SetListHeader( text, pre, pos )
+	--[[ Public functions ]]
+	--
+	function self:SetListHeader(text, pre, pos)
 		self.listheader.text = text
-		if pre != nil then
+
+		if pre ~= nil then
 			self.listheader.textpre = pre
 		end
-		if pos != nil then
+
+		if pos ~= nil then
 			self.listheader.textpos = pos
 		end
 	end
@@ -107,46 +115,53 @@ function PANEL:Init()
 	function self:ReSize()
 		local _tab = self.panels
 		self.height = 0
-		for i, panel in pairs( _tab ) do
+
+		for i, panel in pairs(_tab) do
 			if i > 1 then
 				self.height = self.height + panel:GetTall()
 			else
 				self.height = self.height + panel:GetTall()
 			end
-			panel:SetPos( 0, self.height-panel:GetTall() )
+
+			panel:SetPos(0, self.height - panel:GetTall())
 		end
 	end
 
-	function self:Add( panel )
-		panel:SetParent( self.list )
-		self.list:AddItem( panel )
+	function self:Add(panel)
+		panel:SetParent(self.list)
+		self.list:AddItem(panel)
 	end
 
-	function self:AddEntry( tbl )
-		local _new = createD( "DButton", nil, self:GetWide(), ctr( 50 ), 0, 0 )
-		_new:SetText( "" )
+	function self:AddEntry(tbl)
+		local _new = createD("DButton", nil, self:GetWide(), ctr(50), 0, 0)
+		_new:SetText("")
 		_new.tbl = tbl
-		function _new:Paint( pw, ph )
-			local _color = Color( 255, 255, 255 )
+
+		function _new:Paint(pw, ph)
+			local _color = Color(255, 255, 255)
+
 			if self:IsHovered() or _pnl.uid == self.tbl.uniqueID then
-				_color = Color( 255, 255, 0 )
+				_color = Color(255, 255, 0)
 			end
-			draw.RoundedBox( 0, 0, 0, pw, ph, _color )
-			surfaceText( SQL_STR_OUT( self.tbl.name ), "SettingsNormal", pw/2, ph/2, Color( 255, 255, 255 ), 1, 1 )
+
+			draw.RoundedBox(0, 0, 0, pw, ph, _color)
+			surfaceText(SQL_STR_OUT(self.tbl.name), "SettingsNormal", pw / 2, ph / 2, Color(255, 255, 255), 1, 1)
 		end
+
 		function _new:DoClick()
-			_pnl.edf( self.tbl )
+			_pnl.edf(self.tbl)
 			_pnl.uid = self.tbl.uniqueID
 			_pnl.name = self.tbl.name or "unnamed"
 		end
-		self:Add( _new )
+
+		self:Add(_new)
 	end
 
 	function self:GetEditFunc()
 		return self.edf
 	end
 
-	function self:SetEditFunc( func )
+	function self:SetEditFunc(func)
 		self.edf = func
 	end
 
@@ -154,39 +169,43 @@ function PANEL:Init()
 		return self.editarea
 	end
 
-	function self:SetEditArea( panel )
+	function self:SetEditArea(panel)
 		self.editarea = panel
 	end
 end
 
 function PANEL:Think()
 	local _x = self.listheader:GetPos()
-	if ctr( 60 ) != _x then
-		self.listheader:SetPos( 0, ctr( 60 ) )
+
+	if ctr(60) ~= _x then
+		self.listheader:SetPos(0, ctr(60))
 	end
+
 	_x = self.list:GetPos()
-	if ctr( 110 ) != _x then
-		self.list:SetPos( 0, ctr( 110 ) )
+
+	if ctr(110) ~= _x then
+		self.list:SetPos(0, ctr(110))
 	end
 
-	if self:GetWide() != self.header:GetWide() then
-		self.header:SetWide( self:GetWide() )
-	end
-	if self:GetWide() != self.listheader:GetWide() then
-		self.listheader:SetWide( self:GetWide() )
-	end
-	if self:GetWide() != self.list:GetWide() then
-		self.list:SetWide( self:GetWide() )
+	if self:GetWide() ~= self.header:GetWide() then
+		self.header:SetWide(self:GetWide())
 	end
 
-	if self.list:GetTall() != self:GetTall() - ctr( 120 ) then
-		self.list:SetTall( self:GetTall() - ctr( 120 ) )
+	if self:GetWide() ~= self.listheader:GetWide() then
+		self.listheader:SetWide(self:GetWide())
+	end
+
+	if self:GetWide() ~= self.list:GetWide() then
+		self.list:SetWide(self:GetWide())
+	end
+
+	if self.list:GetTall() ~= self:GetTall() - ctr(120) then
+		self.list:SetTall(self:GetTall() - ctr(120))
 	end
 end
 
-function PANEL:Paint( w, h )
+function PANEL:Paint(w, h)
 	--draw.RoundedBox( 0, 0, 0, w, h, Color( 255, 0, 0 ) )
 end
 
-
-vgui.Register( "DYRPDBList", PANEL, "Panel" )
+vgui.Register("DYRPDBList", PANEL, "Panel")
