23d7a63f508b33a8b573b7a2374081958238155a
D4KiR <23246564+d4kir92@users.noreply.github.com>
2024-01-01 12:17:58 +0100

    fix

--- yourrp/yourrp/gamemodes/yourrp/entities/entities/yrp_chest_32/init.lua
+++ yourrp/yourrp/gamemodes/yourrp/entities/entities/yrp_chest_32/init.lua
@@ -2,17 +2,14 @@
 AddCSLuaFile("cl_init.lua")
 AddCSLuaFile("shared.lua")
 include("shared.lua")
-
 function ENT:SetStorage(id)
 	self._suid = id
 end
 
 function ENT:Use(activator, caller, useType, value)
 	self.d = self.d or 0
-
 	if self.d < CurTime() then
 		self.d = CurTime() + 1
-
 		if self._suid ~= nil then
 			OpenWorldStorage(activator, self._suid, self:GetClass())
 		end
@@ -25,26 +22,26 @@ function ENT:Initialize()
 	self:SetMoveType(MOVETYPE_VPHYSICS)
 	self:SetSolid(SOLID_VPHYSICS)
 	local phys = self:GetPhysicsObject()
-
 	if phys:IsValid() then
 		phys:Wake()
 	end
 
 	self.text_type = "chest"
 	self.bag_size = 32
-
-	timer.Simple(0.1, function()
-		YRPRegisterObject(self)
-	end)
+	timer.Simple(
+		0.1,
+		function()
+			YRPRegisterObject(self)
+		end
+	)
 end
 
 function ENT:Think()
 	self.PermaProps = true
 	self.PermaPropID = 0
-
 	if self:GetPersistent() then
 		self:SetPersistent(false)
-		local msg = "Do NOT persist Teleporters!"
+		local msg = "Do NOT persist (permaprop) Storage containers!"
 		PrintMessage(HUD_PRINTCENTER, msg)
 		YRP.msg("note", msg)
 	end
